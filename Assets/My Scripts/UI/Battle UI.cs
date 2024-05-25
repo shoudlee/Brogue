e0492b860d8015e273cda505e88cd88101789ebe
shoudle <35156944+shoudlee@users.noreply.github.com>
2024-05-25 19:23:52 +0800

    player health portrait

---
+++ Assets/My Scripts/UI/Battle UI.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Brogue.Player;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform healthMask;
+    [SerializeField] private Image normalPlayerPortrait;
+    [SerializeField] private Image deadPlayerPortrait;
+    private PlayerMovement playerMovement;
+    private void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        if (playerMovement == null)
+        {
+            Debug.Log("Player script not found. ");
+        }
+    }
+
+    void Update()
+    {
+        UpdatePlayerPortrait();
+    }
+
+    private float GetPlayerHealthRadio()
+    {
+        
+        return (float)playerMovement.currentHp/playerMovement.playerMaxHp;
+    }
+    private void UpdatePlayerPortrait()
+    {
+        float radio = 1 - GetPlayerHealthRadio();
+        if (radio > 0)
+        {
+            healthMask.sizeDelta = new Vector2(healthMask.sizeDelta.x, radio * 100);
+        }
+        else if (normalPlayerPortrait.IsActive())
+        {
+            normalPlayerPortrait.gameObject.SetActive(false);
+            deadPlayerPortrait.gameObject.SetActive(true);
+        }
+    }
+}
